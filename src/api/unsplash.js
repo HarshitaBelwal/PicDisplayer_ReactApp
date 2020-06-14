8b5fc72b512fd45bc698e8ef3199201c2c5c139a
HarshitaBelwal <belwalharshita@yahoo.in>
2020-06-14 20:29:31 +0530

    add you key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL : 'https://api.unsplash.com',
     headers: {
-        Authorization: "Client-ID -x7KIlZQv5bfsySYcAlvcQaMv47Jq9jvNEhdtgxFSHQ"
+        Authorization: "Client-ID yourKEY"
       }
 });
